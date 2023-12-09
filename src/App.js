74ee7853b1e210e8fd1088c7bf69d903d55b5cff
aleedca <acerdas1701@estudiantec.cr>
2023-12-09 13:50:07 -0600

    first page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './app.css'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+export default function App() {
+    return (
+        <div className='center-screen'>
+        <div className='menu'>
+            <h1>Tic Tac Toe</h1>
+            <h3>Who's there?</h3>
+            <button>One player</button>
+            <button>Two players</button>
+        </div>
+        </div>
+    );
 }
-
-export default App;
